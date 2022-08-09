37f09419259a71e4b08fe5085939e4d0ea69a0ee
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-08-09 14:06:04 +0500

    Chapter one && two. Last lesson: 10

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+interface User {
+    name: string;
+}
+
+const a = {};
+
+assertUser(a);
+a.name = 'Вася';
+
+
+function assertUser(obj: unknown): asserts obj is User {
+    if (typeof obj === 'object' && !!obj && 'name' in obj) {
+        return;
+    }
+    throw new Error('Не пользователь');
+}
+
+function isUser(obj: unknown): obj is User {
+    if (typeof obj === 'object' && !!obj && 'name' in obj) {
+        return true;
+    }
+    return false;
+}
