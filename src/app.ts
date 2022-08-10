e87fa957c4e1af6aca4f04f8ab187986352cc838
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-08-10 10:43:49 +0500

    Chapter one && two. Last lesson: 10

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,40 @@
-interface User {
-    name: string;
+// const skills: string[] = ['Dev', 'DevOps', 'Testing'];
+//
+// for (const skill of skills) {
+//     console.log(skill);
+// }
+//
+// const res = skills
+//     .filter((s: string) => s !== 'DevOps')
+//     .map((s: string) => s + '!')
+//     .reduce((a: string, b: string) => a + b);
+//
+// console.log(res)
+
+// const skill: [number, string] = [22, 'Dev'];
+// const [id, skillName] = skill;
+//
+// const arr: readonly [number, string, ...boolean[]] = [1, 'sdf', true, true, false];
+// const skills: readonly string[] = ['Dev', 'DevOps'];
+
+enum StatusCode {
+    SUCCESS = 1,
+    IN_PROCESS,
+    FAILED
 }
 
-const a = {};
-
-assertUser(a);
-a.name = 'Вася';
-
-
-function assertUser(obj: unknown): asserts obj is User {
-    if (typeof obj === 'object' && !!obj && 'name' in obj) {
-        return;
-    }
-    throw new Error('Не пользователь');
+const res = {
+    message: 'Платеж успешен',
+    statusCode: StatusCode.SUCCESS
 }
 
-function isUser(obj: unknown): obj is User {
-    if (typeof obj === 'object' && !!obj && 'name' in obj) {
-        return true;
-    }
-    return false;
+if (res.statusCode === StatusCode.SUCCESS) {
+    console.log('===');
 }
+
+function action(status: StatusCode) {
+    console.log(status);
+}
+
+action(StatusCode.SUCCESS);
+action(1);
