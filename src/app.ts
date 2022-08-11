9994d1832f856719dd2172042d37f5fa7b82ff9a
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-08-11 16:37:59 +0500

    Chapter one && two. Last lesson: 10

--- src/app.ts
+++ src/app.ts
@@ -39,26 +39,79 @@
 // action(StatusCode.SUCCESS);
 // action(1);
 
-enum QuestionStatus {
-    Published = 'published',
-    Draft = 'draft',
-    Deleted = 'deleted',
+// enum QuestionStatus {
+//     Published = 'published',
+//     Draft = 'draft',
+//     Deleted = 'deleted',
+// }
+//
+// async function getFaqs(req: {
+//     topicId: number;
+//     status: QuestionStatus;
+// }): Promise<{
+//     question: string;
+//     answer: string;
+//     tags: string[];
+//     likes: number;
+//     status: QuestionStatus;
+// }[]> {
+//     const res = await fetch('/faqs', {
+//         method: 'POST',
+//         body: JSON.stringify(req)
+//     });
+//     const data = await res.json();
+//     return data;
+// }
+
+// ToDO: Union type
+// const arr: [string, number] = ['sdf', 1];
+//
+// function logId(id: string | number | boolean) {
+//     if (typeof id === 'string') {
+//         console.log(id.toString())
+//     } else if (typeof id === 'number') {
+//         console.log(id)
+//     } else {
+//         console.log(id)
+//     }
+// }
+//
+// function logError(err: string | string[]): void {
+//     if (Array.isArray(err)) {
+//         console.log(err)
+//     } else {
+//         console.log(err)
+//     }
+// }
+//
+// function logObject(obj: { a: number } | { b: number }): void {
+//     if ('a' in obj) {
+//         obj.a
+//     } else {
+//         console.log(obj.b)
+//     }
+// }
+//
+// function logMultipleIds(a: string | number, b: string | boolean): void {
+//     if (a === b) {
+//         console.log(a)
+//     } else {
+//         console.log(a)
+//     }
+// }
+//
+// logId(1);
+// logId('sdfsd');
+// logId(true);
+
+// ToDo Literals types
+enum RequestType {
+    GET = 'get',
+    POST = 'post'
 }
 
-async function getFaqs(req: {
-    topicId: number;
-    status: QuestionStatus;
-}): Promise<{
-    question: string;
-    answer: string;
-    tags: string[];
-    likes: number;
-    status: QuestionStatus;
-}[]> {
-    const res = await fetch('/faqs', {
-        method: 'POST',
-        body: JSON.stringify(req)
-    });
-    const data = await res.json();
-    return data;
+function fetchWithAuth(url: string, method: 'post' | 'get'): 1 | -1 {
+    return -1
 }
+
+fetchWithAuth('123123', 'post');
