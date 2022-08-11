196bee780cd41c1782111772862b83f9d1259b66
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-08-11 12:09:06 +0500

    Chapter one && two. Last lesson: 10

--- src/app.ts
+++ src/app.ts
@@ -16,25 +16,49 @@
 //
 // const arr: readonly [number, string, ...boolean[]] = [1, 'sdf', true, true, false];
 // const skills: readonly string[] = ['Dev', 'DevOps'];
+//
+// enum StatusCode {
+//     SUCCESS = 1,
+//     IN_PROCESS,
+//     FAILED
+// }
+//
+// const res = {
+//     message: 'Платеж успешен',
+//     statusCode: StatusCode.SUCCESS
+// }
+//
+// if (res.statusCode === StatusCode.SUCCESS) {
+//     console.log('===');
+// }
+//
+// function action(status: StatusCode) {
+//     console.log(status);
+// }
+//
+// action(StatusCode.SUCCESS);
+// action(1);
 
-enum StatusCode {
-    SUCCESS = 1,
-    IN_PROCESS,
-    FAILED
+enum QuestionStatus {
+    Published = 'published',
+    Draft = 'draft',
+    Deleted = 'deleted',
 }
 
-const res = {
-    message: 'Платеж успешен',
-    statusCode: StatusCode.SUCCESS
+async function getFaqs(req: {
+    topicId: number;
+    status: QuestionStatus;
+}): Promise<{
+    question: string;
+    answer: string;
+    tags: string[];
+    likes: number;
+    status: QuestionStatus;
+}[]> {
+    const res = await fetch('/faqs', {
+        method: 'POST',
+        body: JSON.stringify(req)
+    });
+    const data = await res.json();
+    return data;
 }
-
-if (res.statusCode === StatusCode.SUCCESS) {
-    console.log('===');
-}
-
-function action(status: StatusCode) {
-    console.log(status);
-}
-
-action(StatusCode.SUCCESS);
-action(1);
